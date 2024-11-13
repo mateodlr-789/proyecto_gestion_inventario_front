fb244c0b8719104eb9b996a83fc969bf44770d42
Juan Collante <juan.collante@udea.edu.co>
2024-11-12 20:05:21 -0500

    feat(root): use example for kitchen orders manager

---
+++ app/kitchen.tsx
@@ -0,0 +1,13 @@
+// pages/kitchen.tsx
+import React from 'react';
+import KitchenOrders from '../components/kitchenOrders';
+
+const KitchenPage: React.FC = () => {
+    return (
+        <div>
+            <KitchenOrders />
+        </div>
+    );
+};
+
+export default KitchenPage;
